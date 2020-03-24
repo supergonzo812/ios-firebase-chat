c6cf6aeb732a5aa8999bcee1b3c104231b968982
Chris <chris.gonzales.82@gmail.com>
2020-03-24 13:26:57 -0700

    created Xcode workspace with firebase and messageKit pods. Created storyboards and view controllers.  Created segue and keys file.

---
+++ FirebaseChat/FirebaseChat/Resources/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  FirebaseChat
+//
+//  Created by Chris Gonzales on 3/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+
+
+struct Keys {
+    static var messageDetailSegue: String = "MessageDetailSegue"
+}
